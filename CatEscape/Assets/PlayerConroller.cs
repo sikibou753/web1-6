248a5833817b5e4dd685fa245569bda87c2c41b1
sikibou753 <g1727062@st.t-kougei.ac.jp>
2018-06-04 18:13:36 +0900

    最初のコミット

---
+++ CatEscape/Assets/PlayerConroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConroller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+}
